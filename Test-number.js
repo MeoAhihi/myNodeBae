8f0a41c5167fa74bfaf11d1f62549696495946c2
ViPhong <viphongly2804@gmail.com>
2023-05-11 16:19:30 +0700

    convert to function

--- Test-number.js
+++ Test-number.js
@@ -1,13 +1,17 @@
-const prompt = require('prompt-sync')()
-
-var a = prompt("What's the number? ")
-a = +a // parse into Number
-if (Number.isInteger(a)) {
-    if (a % 2) {
-        console.log(a + ' is odd');
-    } else {
-        console.log(a + ' is even');
-    }
-} else {
-    console.log(a + ' is decimal');
+function TestNumber(a) {
+  if (!Number.isInteger(a)) {
+    return a + " is decimal";
+  }
+  if (a % 2) {
+    // 1 <=> true
+    return a + " is odd";
+  }
+  return a + " is even";
 }
+
+const prompt = require("prompt-sync")();
+
+var a = prompt("What's the number? ");
+a = +a; // parse into Number
+
+console.log(TestNumber(a));
